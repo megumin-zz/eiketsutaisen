008b624bf0e991bafcddfb0914191dc5cf606b00
megumi <mg.kn.23@gmail.com>
2022-04-22 12:09:56 +0900

    safari5

--- castle.js
+++ castle.js
@@ -11,10 +11,17 @@ function main() {
         return;
     }
 
-    //   removeAppendedElements();
+    removeAppendedElements();
+
     if (location.href.startsWith("https://eiketsu-taisen.net/members/history/daily")) {
         showDeckAtHistory()
     }
+}
+
+function getMainElement() {
+    const host = document.getElementById('app');
+    const elem = host.getElementsByTagName('ekt-main')[0];
+    return elem.shadowRoot.querySelectorAll('.mb5');
 }
 
 
@@ -23,29 +30,29 @@ function showDeckAtHistory() {
     appendToggleNameButton();
 }
 
+function removeAppendedElements() {
+    // const appendedElementList = getMainElement();
+    // const appendedElementList = wraparea.getElementsByClassName("appended-class");
+    // for (var i = appendedElementList.length - 1; i >= 0; i--) {
+    //     if(appendedElementList[i].getElementsByClassName("appended-class")) {
+    //         appendedElementList[i].remove();
+    //     }
+        
+    // };
+}
+
 
 function appendDeckArea() {
-    // template要素を取得
-
-
-    const host = document.getElementById('app');
-    const elem = host.getElementsByTagName('ekt-main')[0];
-    const wraparea = elem.shadowRoot.querySelectorAll('.mb5');
-
+    const wraparea = getMainElement();
     for (var i = 0; i < wraparea.length; i++) {
         wraparea[i].style.position = "relative";
         wraparea[i].style.height = "135px";
     }
 
-
     [].forEach.call(wraparea, (battleBlock, index) => {
-
         var battleType = battleBlock.getElementsByClassName("ta_r")[0];
-
         const SHOW_DECK_BATTLE_TYPE = ["全国対戦", "群雄伝", "店内対戦", "戦友対戦"];
         if (SHOW_DECK_BATTLE_TYPE.includes(battleType.textContent)) {
-
-
             const myDeckDiv = document.createElement("div");
             myDeckDiv.style.position = "absolute";
             myDeckDiv.style.width = "300px";
@@ -59,12 +66,11 @@ function appendDeckArea() {
 
 
             const myCardTable = document.createElement("div");
-            myCardTable.style.borderCollapse = "separate";
-            myCardTable.style.borderSpacing = "2px";
             const myCardTr = document.createElement("ul");
             myCardTr.style.display = "flex";
             myCardTr.style.justifyContent = "center";
             myCardTr.style.alignContent = "center";
+            myCardTr.style.gap = "6px 4px";
             myCardTr.id = battleBlock.href + "_mydata";
             myCardTable.appendChild(myCardTr);
             myDeckDiv.appendChild(myCardTable);
@@ -84,45 +90,24 @@ function appendDeckArea() {
             enemyGageDiv.classList.add("enemy-gage");
             enemyDeckDiv.appendChild(enemyGageDiv);
 
-            const enemyCardTable = document.createElement("table");
-            enemyCardTable.style.border = "solid 1px #fff";
-            enemyCardTable.style.borderCollapse = "separate";
-            enemyCardTable.style.borderSpacing = "2px";
+            const enemyCardTable = document.createElement("div");
             enemyCardTable.style.margin = "auto";
-            const enemyCardTr = document.createElement("tr");
+            const enemyCardTr = document.createElement("ul");
+            enemyCardTr.style.display = "flex";
+            enemyCardTr.style.gap = "6px 4px";
+            enemyCardTr.style.justifyContent = "center";
+            enemyCardTr.style.alignContent = "center";
             enemyCardTr.id = battleBlock.href + "_enemydata";
             enemyCardTable.appendChild(enemyCardTr);
             enemyDeckDiv.appendChild(enemyCardTable);
 
-
             battleBlock.appendChild(myDeckDiv)
             battleBlock.appendChild(enemyDeckDiv)
-
-
 
             appendDeck(battleBlock)
 
         }
-
     })
-
-
-    //    var fragment = document.createDocumentFragment();
-
-    //    var clone = document.importNode(template.content, true);
-    //        // 複製した各要素に値を設定
-    //        const wraparea = clone.querySelectorAll('.mb5');
-
-    //        console.log(clone)
-
-    // wraparea.style.height = "158px";
-    //     //    wraparea.appendChild(result);
-
-
-
-    //   console.log(historyBlock)
-    //   appendDeckArea(historyBlock);
-    //   appendDeck(historyBlock)
 }
 
 function appendDeck(historyBlock) {
@@ -142,7 +127,6 @@ function appendDeck(historyBlock) {
 
         const template = event.target.responseXML.getElementById('template');
         const mygage = event.target.responseXML.importNode(template.content, true);
-
 
         const playerwraparea = mygage.querySelector('.gauge_base.player');
         const playerchildwraparea = playerwraparea.querySelectorAll('.gauge')[0]
@@ -197,44 +181,6 @@ function appendDeck(historyBlock) {
         for (var i = enemyCardCount - 1; i >= 0; i--) {
             addCardToDeckArea(enemyDeckArea, enemyCardList[i]);
         };
-
-
-        // historyBlock.getElementsByClassName("enemy-gage")[0].appendChild(enemywraparea);
-
-
-
-        // const host = event.target.responseXML.getElementById('app');
-        // const elem = host.getElementsByTagName('ekt-main')[0];
-        // const wraparea = elem.shadowRoot.querySelectorAll('.gauge_base');
-
-
-
-        // var myGage = event.target.responseXML.getElementsByClassName("gauge_base")
-
-        // wraparea.style.left = "30px";
-
-        // if (location.href.startsWith("https://eiketsu-taisen.net/members/history/daily")) {
-        //   historyBlock.getElementsByClassName("my-gage")[0].appendChild(myGage);
-        // }
-
-        // var myCardList = event.target.responseXML.getElementsByClassName("frame_red")[0].getElementsByClassName("data_deck_cardblock_card");
-        // var myCardCount = myCardList.length;
-        // for ( var i = myCardCount - 1; i >= 0 ; i--) {
-        //   addCardToDeckArea(myDeckArea, myCardList[i]);
-        // };
-
-        // var enemyGage = event.target.responseXML.getElementsByClassName("gauge_base enemy")[0]
-        // enemyGage.style.right = "30px";
-
-        // if (location.href.startsWith("https://eiketsu-taisen.net/members/history/daily")) {
-        //   historyBlock.getElementsByClassName("enemy-gage")[0].appendChild(enemyGage);
-        // }
-
-        // var enemyCardList = event.target.responseXML.getElementsByClassName("frame_blue")[0].getElementsByClassName("data_deck_cardblock_card");
-        // var enemyCardCount = enemyCardList.length;
-        // for ( var i = enemyCardCount - 1; i >= 0 ; i--) {
-        //   addCardToDeckArea(enemyDeckArea, enemyCardList[i]);
-        // };
     });
     request.send();
 }
@@ -267,10 +213,7 @@ function addCardToDeckArea(deckArea, card) {
 }
 
 function toggleMyName() {
-    const host = document.getElementById('app');
-    const elem = host.getElementsByTagName('ekt-main')[0];
-    const targetElement = elem.shadowRoot.querySelectorAll(".mb5");
-
+    const targetElement = getMainElement();
     [].forEach.call(targetElement, (mydata) => {
         toggleVisibility(mydata.getElementsByClassName("player")[0].firstElementChild)
     }
@@ -278,25 +221,12 @@ function toggleMyName() {
 }
 
 function toggleEnemyName() {
-    const host = document.getElementById('app');
-    const elem = host.getElementsByTagName('ekt-main')[0];
-    const targetElement = elem.shadowRoot.querySelectorAll(".mb5");
-
+    const targetElement = getMainElement();
     [].forEach.call(targetElement, (mydata) => {
         toggleVisibility(mydata.getElementsByClassName("player")[0].nextElementSibling.firstElementChild)
     }
     );
 }
-
-// function toggleResult() {
-//   toggleElemetnsVisibility(document.getElementsByClassName("battle_list_result"));
-// }
-
-// function toggleElemetnsVisibility(elements) {
-//   [].forEach.call(elements, (element) => {
-//     toggleVisibility(element);
-//   });
-// }
 
 function toggleVisibility(target) {
     if (target.style.visibility != "hidden") {
@@ -318,6 +248,8 @@ function appendToggleNameButton() {
     toggleMyNameButton.style.padding = "13px";
     toggleMyNameButton.style.fontWeight = "bold";
     toggleMyNameButton.style.border = "1px solid gray";
+    toggleMyNameButton.style.color = "black";
+    toggleMyNameButton.style.fontSize = "0.8rem";
 
     const toggleEnemyNameButton = document.createElement("button");
     toggleEnemyNameButton.addEventListener('click', function () {
@@ -330,6 +262,8 @@ function appendToggleNameButton() {
     toggleEnemyNameButton.style.padding = "13px";
     toggleEnemyNameButton.style.fontWeight = "bold";
     toggleEnemyNameButton.style.border = "1px solid gray";
+    toggleEnemyNameButton.style.color = "black";
+    toggleEnemyNameButton.style.fontSize = "0.8rem";
 
     const host = document.getElementById('app');
     const elem = host.getElementsByTagName('ekt-main')[0];
